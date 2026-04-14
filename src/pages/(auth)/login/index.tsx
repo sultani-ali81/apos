18d313cdbfb17c1e9d46f61c25d51c5bea9db7c8
sultani-ali81 <alisultani.373@gmail.com>
2026-04-14 21:47:05 +0430

    Refactor Login component layout and styles

--- src/pages/(auth)/login/index.tsx
+++ src/pages/(auth)/login/index.tsx
@@ -1,24 +1,24 @@
 import LoginForm from "@/components/layout/login-form";
-import Navbar from "@/components/layout/navbar";
+//import Navbar from "@/components/layout/navbar";
 import StatsPanel from "@/components/layout/stats-panel";
 
 export default function Login() {
   return (
     <>
-      <div className="md:hidden">
+      {/* <div className="md:hidden">
         <Navbar
           userName="John Doe"
           avatarUrl="/photos/profile.jpeg"
           onMenuClick={() => console.log("menu")}
         />
-      </div>
+      </div> */}
 
-      <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 bg-gray-100">
+      <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 bg-white">
         <div className="flex items-center justify-center p-8">
           <LoginForm />
         </div>
 
-        <div className="hidden md:flex flex-col justify-between bg-gradient-to-br from-bg-dark to-bg-light text-white p-10">
+        <div className="hidden md:flex flex-col justify-between bg-radial bg-radial from-bg-light to-bg-dark text-white p-10">
           <StatsPanel />
         </div>
       </div>
