f6cf239bf7ea7a3fec515c35f72da3a66c1541bd
sultani-ali81 <alisultani.373@gmail.com>
2026-04-14 21:41:52 +0430

    Change background color classes in AuthLayout

--- src/components/layout/auth-layout.tsx
+++ src/components/layout/auth-layout.tsx
@@ -5,10 +5,10 @@ type Props = {
 
 export default function AuthLayout({ left, right }: Props) {
   return (
-    <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 bg-gray-100">
+    <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 bg-white">
       <div className="flex items-center justify-center p-8">{left}</div>
 
-      <div className=" md:flex flex-col justify-between bg-gradient-to-br from-bg-dark to-bg-light text-white p-10">
+      <div className=" md:flex flex-col justify-between bg-radial from-bg-light to-bg-dark text-white p-10">
         {right}
       </div>
     </div>
